8e6f5dbefcbb27a02ba95c211d76798e2183b656
Kalpesh Deo <kalpesh.dev+1@gmail.com>
2023-09-06 19:32:19 +0530

    feat: Implement Update and Delete Oplog Parsing with Tests
    Implemented parsing for MongoDB update and delete oplog entries, and generate equivalent SQL statements for data insertion into an RDBMS. Added comprehensive unit tests to ensure accuracy and reliability.

--- main.go
+++ main.go
@@ -15,14 +15,28 @@ type OplogEntry struct {
 	Op string                 `json:"op"`
 	NS string                 `json:"ns"`
 	O  map[string]interface{} `json:"o"`
+	O2 map[string]interface{} `json:"o2"`
 }
 
-func GenerateInsertSQL(oplog string) (string, error) {
+func GenerateSQL(oplog string) (string, error) {
 	var oplogObj OplogEntry
 	if err := json.Unmarshal([]byte(oplog), &oplogObj); err != nil {
 		return "", err
 	}
 
+	switch oplogObj.Op {
+	case "i":
+		return generateInsertSQL(oplogObj)
+	case "u":
+		return generateUpdateSQL(oplogObj)
+	case "d":
+		return generateDeleteSQL(oplogObj)
+	}
+
+	return "", fmt.Errorf("invalid oplog")
+}
+
+func generateInsertSQL(oplogObj OplogEntry) (string, error) {
 	switch oplogObj.Op {
 	case "i":
 		sql := fmt.Sprintf("INSERT INTO %s", oplogObj.NS)
@@ -47,6 +61,67 @@ func GenerateInsertSQL(oplog string) (string, error) {
 	return "", nil
 }
 
+func generateUpdateSQL(oplogObj OplogEntry) (string, error) {
+	switch oplogObj.Op {
+	case "u":
+		sql := fmt.Sprintf("UPDATE %s SET", oplogObj.NS)
+
+		diffMap, ok := oplogObj.O["diff"].(map[string]interface{})
+		if !ok {
+			return "", fmt.Errorf("invalid oplog")
+		}
+
+		if setMap, ok := diffMap["u"].(map[string]interface{}); ok {
+			columnValues := make([]string, 0, len(setMap))
+			for columnName, value := range setMap {
+				columnValues = append(columnValues, fmt.Sprintf("%s = %s", columnName, getColumnValue(value)))
+			}
+			sort.Strings(columnValues)
+
+			sql = fmt.Sprintf("%s %s", sql, strings.Join(columnValues, ", "))
+		} else if unsetMap, ok := diffMap["d"].(map[string]interface{}); ok {
+			columnValues := make([]string, 0, len(unsetMap))
+			for columnName := range unsetMap {
+				columnValues = append(columnValues, fmt.Sprintf("%s = NULL", columnName))
+			}
+			sort.Strings(columnValues)
+
+			sql = fmt.Sprintf("%s %s", sql, strings.Join(columnValues, ", "))
+		} else {
+			return "", fmt.Errorf("invalid oplog")
+		}
+
+		whereColumnValues := make([]string, 0, len(oplogObj.O2))
+		for columnName, value := range oplogObj.O2 {
+			whereColumnValues = append(whereColumnValues, fmt.Sprintf("%s = %s", columnName, getColumnValue(value)))
+		}
+
+		sql = fmt.Sprintf("%s WHERE %s;", sql, strings.Join(whereColumnValues, " AND "))
+
+		return sql, nil
+	}
+
+	return "", fmt.Errorf("invalid oplog")
+}
+
+func generateDeleteSQL(oplogObj OplogEntry) (string, error) {
+	switch oplogObj.Op {
+	case "d":
+		// DELETE FROM test.student WHERE _id = '635b79e231d82a8ab1de863b';
+		sql := fmt.Sprintf("DELETE FROM %s WHERE", oplogObj.NS)
+
+		whereColumnValues := make([]string, 0, len(oplogObj.O))
+		for columnName, value := range oplogObj.O {
+			whereColumnValues = append(whereColumnValues, fmt.Sprintf("%s = %s", columnName, getColumnValue(value)))
+		}
+
+		sql = fmt.Sprintf("%s %s;", sql, strings.Join(whereColumnValues, " AND "))
+
+		return sql, nil
+	}
+	return "", fmt.Errorf("invalid oplog")
+}
+
 func getColumnValue(value interface{}) string {
 	switch value.(type) {
 	case int, int8, int16, int32, int64, float32, float64:
@@ -57,8 +132,3 @@ func getColumnValue(value interface{}) string {
 		return fmt.Sprintf("'%v'", value)
 	}
 }
-
-// // happy path:
-// 	oplog -> sql
-// // unhappy path:
-// 	oplog -> error
--- main_test.go
+++ main_test.go
@@ -31,16 +31,89 @@ func TestGenerateInsertSQL(t *testing.T) {
 			want:    "INSERT INTO test.student (_id, date_of_birth, is_graduated, name, roll_no) VALUES ('635b79e231d82a8ab1de863b', '2000-01-30', false, 'Selena Miller', 51);",
 			wantErr: false,
 		},
+		{
+			name: "Update operation - set",
+			oplog: `{
+				"op": "u",
+				"ns": "test.student",
+				"o": {
+				   "$v": 2,
+				   "diff": {
+					  "u": {
+						 "is_graduated": true
+					  }
+				   }
+				},
+				 "o2": {
+				   "_id": "635b79e231d82a8ab1de863b"
+				}
+			}`,
+			want:    "UPDATE test.student SET is_graduated = true WHERE _id = '635b79e231d82a8ab1de863b';",
+			wantErr: false,
+		},
+		{
+			name: "Update operation - set with multiple columns",
+			oplog: `{
+				"op": "u",
+				"ns": "test.student",
+				"o": {
+				   "$v": 2,
+				   "diff": {
+					  "u": {
+						 "is_graduated": true,
+						 "roll_no": 21
+					  }
+				   }
+				},
+				 "o2": {
+				   "_id": "635b79e231d82a8ab1de863b"
+				}
+			}`,
+			want:    "UPDATE test.student SET is_graduated = true, roll_no = 21 WHERE _id = '635b79e231d82a8ab1de863b';",
+			wantErr: false,
+		},
+		{
+			name: "Update operation - unset",
+			oplog: `{
+				"op": "u",
+				"ns": "test.student",
+				"o": {
+				   "$v": 2,
+				   "diff": {
+					  "d": {
+						 "roll_no": false
+					  }
+				   }
+				},
+				"o2": {
+				   "_id": "635b79e231d82a8ab1de863b"
+				}
+			}`,
+			want:    "UPDATE test.student SET roll_no = NULL WHERE _id = '635b79e231d82a8ab1de863b';",
+			wantErr: false,
+		},
+		{
+			name: "Delete operation",
+			oplog: `{
+				"op": "d",
+				"ns": "test.student",
+				"o": {
+				  "_id": "635b79e231d82a8ab1de863b"
+				}
+			  }`,
+			want:    "DELETE FROM test.student WHERE _id = '635b79e231d82a8ab1de863b';",
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GenerateInsertSQL(tt.oplog)
+			got, err := GenerateSQL(tt.oplog)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("GenerateInsertSQL() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("GenerateSQL() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
-				t.Errorf("GenerateInsertSQL() = %v, want %v", got, tt.want)
+				t.Errorf("GenerateSQL() = %v, want %v", got, tt.want)
 			}
 		})
 	}
